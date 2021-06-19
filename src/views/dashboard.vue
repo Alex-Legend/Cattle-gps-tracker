8d4e61a61d4d509acd303a4c2054c44ce7082c53
Alex-Legend <alexpraise753@gmail.com>
2021-06-19 13:02:12 +0200

    thank you Chawe

--- src/views/dashboard.vue
+++ src/views/dashboard.vue
@@ -1,6 +1,19 @@
 <template>
-    <mdb-container class="container-fluid mt-5">
-        <h1 class="blue lighten-5 black-text text-center">Cattle Tracking Map</h1>
+    <mdb-container Fluid class="pt-5">
+        <mdb-navbar position="top" color="primary">
+			<mdb-navbar-brand to="/">
+			  <mdb-icon class="white-text" icon="home"/>
+			</mdb-navbar-brand>
+			<mdb-navbar-toggler>
+				<mdb-navbar-nav right>
+					<mdb-form-inline>
+						<mdb-btn outline="white" class="" size="sm" @click.native="login = true">Log In</mdb-btn>
+
+					</mdb-form-inline>
+				</mdb-navbar-nav>
+			</mdb-navbar-toggler>
+		</mdb-navbar>
+    <p class="h4 text-center  my-4  blue lighten-5">import components of navbar</p>
        
 
         <div class="card p-1 blue-grey mt-5 w-100">
